8dde5f2a382cbce38287c9381e404e568101abea
shordal <oneholy@hotmail.com>
2020-01-02 20:33:21 -0600

    Add private routes

--- src/filebase/filebase.js
+++ src/filebase/filebase.js
@@ -1,4 +1,5 @@
 import * as firebase from 'firebase';
+
 const config = {
     apiKey: process.env.FIREBASE_API_KEY,
     authDomain: process.env.FIREBASE_AUTH_DOMAIN,
@@ -13,8 +14,9 @@ const config = {
 firebase.initializeApp(config);
 
 const database = firebase.database();
+const googleAuthProvider = new firebase.auth.GoogleAuthProvider();
 
-export { firebase, database as default };
+export { firebase, googleAuthProvider, database as default };
 
 // database.ref('expenses').on('child_removed', (snapshot) => {
 //     console.log(snapshot.key, snapshot.val());
--- src/tests/components/Header.test.js
+++ src/tests/components/Header.test.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-import Header from '../../components/Header';
+import { Header } from '../../components/Header';
 
 test('should render Header correctly', () => {
-    const wrapper = shallow(<Header />);
+    const wrapper = shallow(<Header startLogout={() => { }}/>);
     expect(wrapper).toMatchSnapshot();
 
     // const renderer = new ReactShallowRenderer();
@@ -11,4 +11,9 @@ test('should render Header correctly', () => {
     // expect(renderer.getRenderOutput()).toMatchSnapshot();
 });
 
-
+test('should call startLogout on button click', () => {
+const startLogout = jest.fn(); //spy
+const wrapper = shallow(<Header startLogout={startLogout}/>)
+wrapper.find('button').simulate('click');
+expect(startLogout).toHaveBeenCalled();
+});
